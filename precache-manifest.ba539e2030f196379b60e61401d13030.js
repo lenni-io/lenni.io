ded8a34c29fe5a6e021265720b6d1e3578203e5b
lennitio <infolennart@gmail.com>
2019-04-30 09:48:49 +0200

    Updates

---
+++ precache-manifest.ba539e2030f196379b60e61401d13030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a39a15f32ef362ad12996332a65dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2285a5436b6a541b361",
+    "url": "/static/css/main.fb1a889e.chunk.css"
+  },
+  {
+    "revision": "4d28c9c8f4958798a636",
+    "url": "/static/js/2.ce208153.chunk.js"
+  },
+  {
+    "revision": "a2285a5436b6a541b361",
+    "url": "/static/js/main.04f0c019.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
